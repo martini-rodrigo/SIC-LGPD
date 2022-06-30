fd4af082abb9997ee1e6a167003acbe5081f3690
martini-rodrigo <rodrigocostamartini@gmail.com>
2022-06-30 13:42:36 -0300

    autocomplete

--- scripts/script.js
+++ scripts/script.js
@@ -8,8 +8,27 @@ $(function () {
     });
     $('#import-footer').load("footer.html");
 
-    $('#modal').modal(options);
-
     $('.dropdown-toggle').dropdown();
+
+    var data = [
+        "SECRETARIA DOS TRANSPORTES METROPOLITANOS",
+        "SECRETARIA DE LOGÍSTICA E TRANSPORTES",
+        "SECRETARIA DA JUSTIÇA E CIDADANIA",
+        "SECRETARIA DA SAÚDE",
+        "SECRETARIA DA HABITAÇÃO",
+        "SEC. DOS DIREITOS DA PESSOA COM DEFICIÊNCIA",
+        "SECRETARIA DA CULTURA E ECONOMIA",
+        "SECRETARIA DE DESENVOLVIMENTO REGIONAL",
+        "SECRETARIA DE TURISMO",
+        "SECRETARIA DA SEGURANÇA PÚBLICA",
+        "SECRETARIA DA ADMINISTRAÇÃO PENITENCIÁRIA",
+        "CIA.PAULISTA DE SECURITIZAÇÃO-CP",
+        "SECRETARIA DE ESPORTES"
+    ];
+    $("#autocomplete").autocomplete({
+        source: data, // specify the data source
+        minLength: 1 // minimum number of characters a user must type before a search   
+    });
+    //$('#modal').modal(options);
 });
 
